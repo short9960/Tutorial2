53022be2084d3f3bc3bb438938297b0459decfd4
short9960 <89544170+short9960@users.noreply.github.com>
2021-10-22 19:02:08 -0400

    First commit

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    public GameObject player;
 
-    public GameObject target;
-
+    private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+        transform.position = player.transform.position + offset;
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,21 +6,97 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
+    private int livesValue = 3;
+    private bool facingRight = true;
 
+    public int scoreValue = 0;
+    public float horizontalValue;
     public float speed;
-
     public Text score;
-
-    private int scoreValue = 0;
-
+    public Text lives;
+    public GameObject player;
     public GameObject winTextObject;
+    public GameObject loseTextObject;
+    public Transform Target;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
+    public AudioSource musicSource2;
+    public float volume = 0.5f;
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+            musicSource.loop = true;
+        }
+
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Score: " + scoreValue.ToString();
+        lives.text = "Lives: " + livesValue.ToString();
         winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        move();
+        Flip();
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (scoreValue == 4)
+        {
+            transform.position = Target.position;
+            livesValue = 3;
+            lives.text = livesValue.ToString();
+        }
+
+        if (scoreValue == 8)
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Stop();
+        }
+
+        if (scoreValue == 8)
+        {
+            musicSource2.PlayOneShot(musicClipTwo, volume);
+            musicSource2.loop = false;
+        }
     }
 
     // Update is called once per frame
@@ -42,17 +118,47 @@ public class PlayerScript : MonoBehaviour
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
         }
 
-        if (scoreValue == 10)
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+        if (scoreValue == 8)
         {
             // display win text
             winTextObject.SetActive(true);
 
         }
 
+        if (livesValue == 0)
+        {
+            // display lose text
+            loseTextObject.SetActive(true);
+
+            Destroy(player);
+
+        }
+    }
+
+    void Flip()
+    {
+        if ((horizontalValue < 0 && facingRight) || (horizontalValue > 0 && !facingRight))
+        {
+            facingRight = !facingRight;
+            transform.Rotate(new Vector3(0, 180, 0));
+        }
+    }
+
+    void move()
+    {
+        horizontalValue = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        //transform.Translate(new Vector3()
     }
 
     private void OnCollisionStay2D(Collision2D collision)
